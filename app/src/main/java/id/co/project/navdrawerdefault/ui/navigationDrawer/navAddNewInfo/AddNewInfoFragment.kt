5310a9ff42947ef8da145d077356a9b580385b1e
iqbal <iqbalthamrin200@gmail.com>
2022-09-19 15:09:53 +0800

    add new layout

--- app/src/main/java/id/co/project/navdrawerdefault/ui/navigationDrawer/navAddNewInfo/AddNewInfoFragment.kt
+++ app/src/main/java/id/co/project/navdrawerdefault/ui/navigationDrawer/navAddNewInfo/AddNewInfoFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import id.co.project.navdrawerdefault.R
 import id.co.project.navdrawerdefault.databinding.FragmentHomeBinding
 
 
@@ -22,6 +26,19 @@ class AddNewInfoFragment : Fragment() {
         val root: View = binding.root
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    //        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+
+        val item = listOf("Aplikasi", "Arsitektur", "Design Interior", "Desain Komunikasi Visual", "Design Produk","Fesyen","Film Animasi Dan Video", "Fotografi","Kria","Kuliner","Musik","Penerbitan","Pengembang Permainan","Periklanan","Seni Pertunjukan", "Seni Rupa", "TV dan Radio")
+        val adapter = ArrayAdapter(requireActivity(), R.layout.list_item_subsektor,item)
+        binding.dropdownMenuAdd.setAdapter(adapter)
+        binding.buttonUpdate.setOnClickListener {
+            Toast.makeText(requireActivity(), "Data Berhasil Di Simpan", Toast.LENGTH_SHORT).show()
+        }
+
     }
     override fun onDestroyView() {
         super.onDestroyView()
